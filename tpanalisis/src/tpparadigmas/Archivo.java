89a307fe9ee3908f25596f85ccb02b144d6d996a
arielEliasPerez <ariel65_ep@hotmail.com>
2023-06-11 23:55:02 -0300

    se agrega la escritura de Archivo

--- tpanalisis/src/tpparadigmas/Archivo.java
+++ tpanalisis/src/tpparadigmas/Archivo.java
@@ -1,8 +1,12 @@
 package tpparadigmas;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.io.*;
 
 public class Archivo {
 
@@ -12,96 +16,87 @@ public class Archivo {
 	public Archivo(String ruta) {
 		this.ruta = ruta;
 	}
-	
-	
-	public LinkedList<Atraccion> leerArchivoAtracciones()
-	{
+
+	public LinkedList<Atraccion> leerArchivoAtracciones() {
 		Scanner scanner = null;
 		LinkedList<Atraccion> atracciones = new LinkedList<Atraccion>();
-		
+
 		String nombreAtr;
 		int costo;
 		double tiempo;
 		int cupo;
 		String nombreTipoAtraccion;
-		
-		try 
-		{
+
+		try {
 			File file = new File(this.ruta);
 			scanner = new Scanner(file);
-			
-			
-			while(scanner.hasNext())
-			{
+
+			while (scanner.hasNext()) {
 				nombreAtr = scanner.next();
 				costo = scanner.nextInt();
 				tiempo = scanner.nextDouble();
 				cupo = scanner.nextInt();
 				nombreTipoAtraccion = scanner.next();
-				
-				Atraccion atraccionAct = new Atraccion(nombreAtr,costo,tiempo,TipoAtraccion.asignarTipo(nombreTipoAtraccion),cupo);			
+
+				Atraccion atraccionAct = new Atraccion(nombreAtr, costo, tiempo,
+						TipoAtraccion.asignarTipo(nombreTipoAtraccion), cupo);
 				atracciones.add(atraccionAct);
 			}
-			
-		}
-		catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			scanner.close();
 		}
 		return atracciones;
 	}
-	
-	public LinkedList<Promocion> leerArchivoPromociones(LinkedList<Atraccion>listaAtracciones)
-	{
-		LinkedList<Promocion> listaProds = new LinkedList<Promocion>();		
+
+	public LinkedList<Promocion> leerArchivoPromociones(LinkedList<Atraccion> listaAtracciones) {
+		LinkedList<Promocion> listaProds = new LinkedList<Promocion>();
 		Scanner scanner = null;
 		int tipoPaquete;
 		String nombrePaquete;
 		String nombreTipoAtraccion;
 		int cantAtracciones;
-		TipoAtraccion tipo = null;	
+		TipoAtraccion tipo = null;
 		String nombreAtrac;
 		Atraccion atraccAct = null;
-		
+
 		try {
 			File file = new File(this.ruta);
 			scanner = new Scanner(file);
 			Promocion promoActual = null;
-			
-			while(scanner.hasNext())
-			{
+
+			while (scanner.hasNext()) {
 				nombrePaquete = scanner.next();
 				tipoPaquete = scanner.nextInt();
 				nombreTipoAtraccion = scanner.next();
-				
+
 				tipo = TipoAtraccion.asignarTipo(nombreTipoAtraccion);
-				
-				switch(tipoPaquete)
-				{
-					case 1:			//Promocion de tipo ABSOLUTO
-						promoActual = new PromocionAbsoluta(nombrePaquete,tipo,scanner.nextInt());
-						break;
-						
-					case 2:			//Promocion de tipo PORCENTUAL
-						promoActual = new PromocionPorcentual(nombrePaquete,tipo,scanner.nextDouble());
-						break;
-						
-					case 3:			//Promocion de tipo AxB
-						String nombreAtr = scanner.next(); 
-						
-						atraccAct = Atraccion.buscarAtraccionEnLista(nombreAtr,nombrePaquete, listaAtracciones);
-						
-						promoActual = new PromocionAxB(nombrePaquete,tipo,atraccAct);
-						break;
+
+				switch (tipoPaquete) {
+				case 1: // Promocion de tipo ABSOLUTO
+					promoActual = new PromocionAbsoluta(nombrePaquete, tipo, scanner.nextInt());
+					break;
+
+				case 2: // Promocion de tipo PORCENTUAL
+					promoActual = new PromocionPorcentual(nombrePaquete, tipo, scanner.nextDouble());
+					break;
+
+				case 3: // Promocion de tipo AxB
+					String nombreAtr = scanner.next();
+
+					atraccAct = Atraccion.buscarAtraccionEnLista(nombreAtr, nombrePaquete, listaAtracciones);
+
+					promoActual = new PromocionAxB(nombrePaquete, tipo, atraccAct);
+					break;
 				}
-				
+
 				cantAtracciones = scanner.nextInt();
-			
-				for(int i=0;i<cantAtracciones;i++)
-				{
+
+				for (int i = 0; i < cantAtracciones; i++) {
 					nombreAtrac = scanner.next();
-					atraccAct = Atraccion.buscarAtraccionEnLista(nombreAtrac,nombrePaquete, listaAtracciones);
+					atraccAct = Atraccion.buscarAtraccionEnLista(nombreAtrac, nombrePaquete, listaAtracciones);
 					promoActual.addAtraccion(atraccAct);
 				}
 				listaProds.add(promoActual);
@@ -110,49 +105,72 @@ public class Archivo {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			
+
 			scanner.close();
 		}
 		return listaProds;
 	}
 
-	public LinkedList<Usuario> leerArchivoUsuarios()
-	{
+	public LinkedList<Usuario> leerArchivoUsuarios() {
 		LinkedList<Usuario> colaUsuarios = new LinkedList<Usuario>();
-		
+
 		Scanner scanner = null;
 		String nombre;
 		int presupuesto;
 		double tiempoDisp;
 		TipoAtraccion preferencia;
 		Usuario usuarioAct;
-		try
-		{
+		try {
 			File file = new File(this.ruta);
 			scanner = new Scanner(file);
-			
-			
-			while(scanner.hasNext())
-			{
+
+			while (scanner.hasNext()) {
 				nombre = scanner.next();
-				
+
 				preferencia = TipoAtraccion.asignarTipo(scanner.next());
-				
+
 				presupuesto = scanner.nextInt();
-				
+
 				tiempoDisp = scanner.nextDouble();
-				
-				usuarioAct = new Usuario(nombre,presupuesto,tiempoDisp,preferencia);
-				
+
+				usuarioAct = new Usuario(nombre, presupuesto, tiempoDisp, preferencia);
+
 				colaUsuarios.add(usuarioAct);
-			}	
-		}catch (Exception e) {
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			scanner.close();
 		}
-		
+
 		return colaUsuarios;
 	}
 
+	public void guardarArchivoCompras( ArrayList<Compra> compras) {
+		
+		FileWriter file = null; 
+		PrintWriter escritura; 
+	
+		try{
+			file = new FileWriter( this.ruta );
+			escritura = new PrintWriter(file); //Referenciamos enviando el obj file donde queremos escribir.
+			
+			//Ahora sí, escrimos dato por dato:
+			for(Compra compra : compras) {
+				escritura.println(compra);
+			}
+		} catch( Exception e) {
+			e.printStackTrace();
+		} finally {
+			if( file!= null ) {
+				try {
+					file.close();
+				} catch( IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Archivo de Compras procesado");
+	}
 }
+	

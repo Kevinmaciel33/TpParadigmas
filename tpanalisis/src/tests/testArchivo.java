953eb4508605489127201497cdf3e25c9dc13bd3
kevin-maciel <83225077+kevin-maciel@users.noreply.github.com>
2023-06-12 00:25:07 -0300

    add test1

--- tpanalisis/src/tests/testArchivo.java
+++ tpanalisis/src/tests/testArchivo.java
@@ -5,10 +5,12 @@ import java.util.LinkedList;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
 import tpparadigmas.Archivo;
 import tpparadigmas.Atraccion;
 import tpparadigmas.Producto;
 import tpparadigmas.Promocion;
+import tpparadigmas.TipoAtraccion;
 import tpparadigmas.Usuario;
 
 public class testArchivo {
@@ -44,4 +46,37 @@ public class testArchivo {
 		
 		System.out.println(listaUsuarios);
 	}
+	
+	@Test
+	public void obtenerAtracciones() throws Exception 
+	{
+		LinkedList<Atraccion> listaAtracciones = new LinkedList<Atraccion>();
+		
+		Atraccion attr1 = new Atraccion("Paraiso", 10, 50, TipoAtraccion.Degustacion, 1);
+		Atraccion attr2 = new Atraccion("Tierra", 10, 50, TipoAtraccion.Aventura, 1);
+		Atraccion attr3 = new Atraccion("Infierno", 10, 50, TipoAtraccion.Aventura, 1);
+		
+		listaAtracciones.add(attr1);
+		listaAtracciones.add(attr2);
+		listaAtracciones.add(attr3);
+		
+		String aBuscar = "Paraiso";
+		 Atraccion encontrada = Atraccion.buscarAtraccionEnLista("Paraiso", "", listaAtracciones);
+		
+		 assertEquals(aBuscar,encontrada.getNombre());
+	}
+	
+	@Test
+	public void AtraccionSinCupo() 
+	{
+		Atraccion attr = new Atraccion("L", 10, 50, TipoAtraccion.Aventura, 1);
+		
+		attr.decrementarCupo();
+		
+		boolean tieneCupo = attr.sinCupo();
+		
+		assertEquals(true, tieneCupo);
+	}
+	
+	
 }
